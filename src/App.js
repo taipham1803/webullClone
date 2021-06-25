5cee2933619989a78b4074c3976f6c91313f9cfd
TaiPham <taipham1803@gmail.com>
2021-06-25 14:31:08 +0700

    upload project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import GlobalStyle from './globalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Navbar />
+      <Hero />
+    </>
+  );
+}
+
+export default App;
